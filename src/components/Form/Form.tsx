1dc02e5ae866ce7b8a147aa53556aa98f54511b7
Maximiliano Gutierrez <hmgutierrezfernandez@gmail.com>
2023-01-31 20:44:46 +0100

    Create form component

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+const Form = () => {
+  const [take, setTake] = useState();
+
+  return (
+    <form className="search-form">
+      <div className="row">
+        <label htmlFor="search" className="col-1 col-form-label">
+          Search:{" "}
+        </label>
+        <div className="col-4">
+          <input type="text" id="search" className="search form-control" />
+        </div>
+        <button type="submit" className="btn btn-info btn-sm col-1">
+          Go
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
